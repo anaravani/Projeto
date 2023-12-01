486639af7e2df5d0fb83323f3cb55f94f058cf9d
Lívia <livia.caroline@portalsesisp.org.br>
2023-12-01 11:27:42 -0300

    0112

--- Areas/Admin/Controllers/AdminItemController.cs
+++ Areas/Admin/Controllers/AdminItemController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Projeto.Context;
 using Projeto.Models;
+using ReflectionIT.Mvc.Paging;
 
 namespace Projeto.Areas.Admin.Controllers
 {
@@ -14,17 +16,40 @@ namespace Projeto.Areas.Admin.Controllers
     public class AdminItemController : Controller
     {
         private readonly AppDbContext _context;
+        private readonly ConfiguraImagem _confImg;
+        private readonly IWebHostEnvironment _hostingEnvireoment;
 
-        public AdminItemController(AppDbContext context)
+        public AdminItemController(AppDbContext context,
+IWebHostEnvironment hostEnvironment, IOptions<ConfiguraImagem> confImg)
         {
             _context = context;
+            _confImg = confImg.Value;
+            _hostingEnvireoment = hostEnvironment;
         }
-
         // GET: Admin/AdminItem
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string filtro, int pageindex = 1,
+string sort = "Nome")
         {
-            var appDbContext = _context.Itens.Include(i => i.Categoria);
-            return View(await appDbContext.ToListAsync());
+            var itenslist =
+
+            _context.Itens.AsNoTracking().AsQueryable();
+
+            if (filtro != null)
+            {
+                itenslist = itenslist.Where(p =>
+
+                p.Nome.Contains(filtro));
+
+            }
+            var model = await PagingList.CreateAsync(itenslist, 5,
+
+            pageindex, sort, "Nome");
+
+            model.RouteValue = new RouteValueDictionary{{"filtro", filtro
+
+}};
+
+            return View(model);
         }
 
         // GET: Admin/AdminItem/Details/5
@@ -58,8 +83,19 @@ namespace Projeto.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ItemId,Nome,DescricaoCurta,DescricaoDetalhada,Preco,ImagemPequenaUrl,ImagemUrl,Ativo,Destaque,Categoriaid")] Item item)
+        public async Task<IActionResult> Create([Bind("ItemId,Nome,DescricaoCurta,DescricaoDetalhada,Preco,ImagemPequenaUrl,ImagemUrl,Ativo,Destaque,Categoriaid")] Item item, IFormFile Imagem, IFormFile Imagemcurta)
         {
+            if (Imagem != null)
+            {
+                string imagemr = await SalvarArquivo(Imagem);
+                item.ImagemUrl = imagemr;
+            }
+            if (Imagemcurta != null)
+            {
+                string imagemcr = await SalvarArquivo(Imagemcurta);
+                item.ImagemPequenaUrl = imagemcr;
+            }
+
             if (ModelState.IsValid)
             {
                 _context.Add(item);
@@ -92,11 +128,25 @@ namespace Projeto.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ItemId,Nome,DescricaoCurta,DescricaoDetalhada,Preco,ImagemPequenaUrl,ImagemUrl,Ativo,Destaque,Categoriaid")] Item item)
+        public async Task<IActionResult> Edit(int id, [Bind("ItemId,Nome,DescricaoCurta,DescricaoDetalhada,Preco,ImagemPequenaUrl,ImagemUrl,Ativo,Destaque,Categoriaid")] Item item, IFormFile Imagem, IFormFile
+Imagemcurta)
         {
             if (id != item.ItemId)
             {
                 return NotFound();
+            }
+
+            if (Imagem != null)
+            {
+                Deletefile(item.ImagemUrl);
+                string imagemr = await SalvarArquivo(Imagem);
+                item.ImagemUrl = imagemr;
+            }
+            if (Imagemcurta != null)
+            {
+                Deletefile(item.ImagemPequenaUrl);
+                string imagemcr = await SalvarArquivo(Imagemcurta);
+                item.ImagemPequenaUrl = imagemcr;
             }
 
             if (ModelState.IsValid)
@@ -154,16 +204,68 @@ namespace Projeto.Areas.Admin.Controllers
             var item = await _context.Itens.FindAsync(id);
             if (item != null)
             {
+                Deletefile(item.ImagemPequenaUrl);
+                Deletefile(item.ImagemUrl);
                 _context.Itens.Remove(item);
             }
-            
+
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
         private bool ItemExists(int id)
         {
-          return (_context.Itens?.Any(e => e.ItemId == id)).GetValueOrDefault();
+            return (_context.Itens?.Any(e => e.ItemId == id)).GetValueOrDefault();
+        }
+
+        public async Task<string> SalvarArquivo(IFormFile Imagem)
+        {
+            var filePath = Path.Combine(_hostingEnvireoment.WebRootPath,
+
+            _confImg.NomePastaImagemItem);
+
+            if (Imagem.FileName.Contains(".jpg") || Imagem.FileName.Contains(".gif")
+
+            || Imagem.FileName.Contains(".svg") || Imagem.FileName.Contains(".png"))
+
+            {
+                string novoNome =
+
+                $"{Guid.NewGuid()}.{Path.GetExtension(Imagem.FileName)}";
+
+                var fileNameWithPath = string.Concat(filePath, "\\", novoNome);
+                using (var stream = new FileStream(fileNameWithPath,
+
+                FileMode.Create))
+                {
+                    await Imagem.CopyToAsync(stream);
+                }
+                return "~/" + _confImg.NomePastaImagemItem + "/" + novoNome;
+            }
+            return null;
+        }
+        public void Deletefile(string fname)
+        {
+            if (fname != null)
+            {
+
+                int pi = fname.LastIndexOf("/") + 1;
+                int pf = fname.Length - pi;
+                string nomearquivo = fname.Substring(pi, pf);
+                try
+                {
+                    string _imagemDeleta = Path.Combine(_hostingEnvireoment.WebRootPath,
+                    _confImg.NomePastaImagemItem + "\\", nomearquivo);
+                    if ((System.IO.File.Exists(_imagemDeleta)))
+                    {
+                        System.IO.File.Delete(_imagemDeleta);
+                    }
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
         }
     }
 }
